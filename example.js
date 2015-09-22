05f26780d0309510060e24e922429add29e83c8e
Henry Henderson <henry925@gmail.com>
2015-09-22 16:37:56 -0400

    Play button

--- example.js
+++ example.js
@@ -1,4 +1,4 @@
-var renderer = PIXI.autoDetectRenderer(1024, 678,{backgroundColor : 0x1099bb});
+var renderer = PIXI.autoDetectRenderer(1000, 750,{backgroundColor : 0x1099bb});
 document.body.appendChild(renderer.view);
 
 // create the root of the scene graph
@@ -18,10 +18,38 @@ bunny.anchor.y = 0.5;
 bunny.position.x = 200;
 bunny.position.y = 150;
 
-stage.addChild(bunny);
+//stage.addChild(bunny);
+
+var graphics = new PIXI.Graphics();
+
+// draw a rounded rectangle
+graphics.lineStyle(2, 0x242124, 1);
+graphics.beginFill(0xFF00BB, 0.25);
+graphics.drawRoundedRect(350, 350, 300, 100, 15);
+graphics.endFill();
+graphics.interactive = true;
+stage.addChild(graphics);
 
 var style = {
-    font : 'bold 18px Delitsch',
+    font : 'bold 50px Verdana',
+    fill : '#ffffff',
+};
+
+var playText = new PIXI.Text('PLAY', style);
+playText.x = 430;
+playText.y = 365;
+stage.addChild(playText);
+
+
+graphics.on('mousedown', onDown);
+
+function onDown (eventData)
+{
+    console.log("I was pressed.");
+}
+
+var style = {
+    font : 'bold 50px Delitsch',
     fill : '#F7EDCA',
 //    dropShadow : true,
 //    dropShadowColor : '#000000',
@@ -31,8 +59,8 @@ var style = {
     wordWrapWidth : 400
 };
 
-var richText = new PIXI.Text('Yo, our enemies be at -5, we are at 1293. SINK THEIR SHIP!', style);
-richText.x = 650;
+var richText = new PIXI.Text('Ye Linelubbers!', style);
+richText.x = 310;
 richText.y = 45;
 
 stage.addChild(richText);
