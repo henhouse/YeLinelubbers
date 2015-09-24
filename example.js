56116a864d9c0c23e089c324f72a68c444be7d85
Henry <henry925@gmail.com>
2015-09-23 23:46:51 -0400

    Organize a bunch of things.
    Place scenes into functions. Call stuff once page has fully loaded.

--- example.js
+++ example.js
@@ -1,77 +1,77 @@
-var renderer = PIXI.autoDetectRenderer(1000, 750,{backgroundColor : 0x1099bb});
-document.body.appendChild(renderer.view);
+var stage, renderer;
 
-// create the root of the scene graph
-var stage = new PIXI.Container();
-
-// create a texture from an image path
-var texture = PIXI.Texture.fromImage('ship_final.png');
-
-// create a new Sprite using the texture
-var bunny = new PIXI.Sprite(texture);
-
-// center the sprite's anchor point
-bunny.anchor.x = 0.5;
-bunny.anchor.y = 0.5;
-
-// move the sprite to the center of the screen
-bunny.position.x = 200;
-bunny.position.y = 150;
-
-stage.addChild(bunny);
-
-var graphics = new PIXI.Graphics();
-
-// draw a rounded rectangle
-graphics.lineStyle(2, 0x242124, 1);
-graphics.beginFill(0xFF00BB, 0.25);
-graphics.drawRoundedRect(350, 350, 300, 100, 15);
-graphics.endFill();
-graphics.interactive = true;
-stage.addChild(graphics);
-
-var style = {
-    font : 'bold 50px Verdana',
-    fill : '#ffffff',
-};
-
-var playText = new PIXI.Text('PLAY', style);
-playText.x = 430;
-playText.y = 365;
-stage.addChild(playText);
-
-var startText = new PIXI.Text('Welcome to Linelubbers!', style);
-startText.x = 200;
-startText.y = 200;
-stage.addChild(startText);
-
-
-graphics.on('mousedown', onDown);
-
-function onDown (eventData)
+function startMenu()
 {
-    generateNumberLine();
-    stage.removeChild(graphics);
-    stage.addChild(richText);
-    stage.removeChild(playText);
-    stage.removeChild(startText);
+    var startButton = new PIXI.Graphics();
+
+    // draw a rounded rectangle
+    startButton.lineStyle(2, 0x242124, 1);
+    startButton.beginFill(0xFF00BB, 0.25);
+    startButton.drawRoundedRect(350, 350, 300, 100, 15);
+    startButton.endFill();
+    startButton.interactive = true;
+    stage.addChild(startButton);
+
+    var style = {
+        font : 'bold 50px Chelsea Market',
+        fill : '#ffffff',
+    };
+    var playText = new PIXI.Text('PLAY', style);
+    playText.x = 430;
+    playText.y = 365;
+    stage.addChild(playText);
+
+
+    var titleText = new PIXI.Text('Welcome to Linelubbers!', style);
+    titleText.x = 200;
+    titleText.y = 200;
+    stage.addChild(titleText);
+
+    startButton.on('mousedown', onDown);
+
+    function onDown(eventData)
+    {
+        generateNumberLine();
+        stage.removeChild(startButton);
+        stage.removeChild(playText);
+        stage.removeChild(titleText);
+        testScene();
+    }
 }
 
-var style = {
-    font : 'bold 25px Delitsch',
-    fill : '#F7EDCA',
-//    dropShadow : true,
-//    dropShadowColor : '#000000',
-//    dropShadowAngle : Math.PI / 6,
-//    dropShadowDistance : 3,
-    wordWrap : true,
-    wordWrapWidth : 350
-};
+function testScene()
+{
+    // create a texture from an image path
+    var shipTexture = PIXI.Texture.fromImage('ship_final.png');
 
-var richText = new PIXI.Text('Yar! Our enemies be at -5, we are at 10. SINK THEIR SHIP!', style);
-richText.position.x = 650;
-richText.position.y = 45;
+    // create a new Sprite using the texture
+    var ship = new PIXI.Sprite(shipTexture);
+    stage.addChild(ship);
 
+    // center the sprite's anchor point
+    ship.anchor.x = 0.5;
+    ship.anchor.y = 0.5;
+
+    // move the sprite to the center of the screen
+    ship.position.x = 20;
+    ship.position.y = 590;
+
+    var style = {
+        font : 'bold 25px Helvetica',
+        fill : '#F7EDCA',
+        dropShadow : true,
+        dropShadowColor : '#000000',
+        dropShadowAngle : Math.PI / 6,
+        dropShadowDistance : 2,
+        wordWrap : true,
+        wordWrapWidth : 345
+    };
+
+    var questionText = new PIXI.Text('Yar! Our enemies be at -5, we are at 10. I won\'t rest until I see a hanging from the yardarm!', style);
+    questionText.position.x = 650;
+    questionText.position.y = 45;
+    stage.addChild(questionText);
+    }
 
 function generateNumberLine(){
     // create a new graphics object
@@ -79,7 +79,7 @@ function generateNumberLine(){
     var linexStart = 20;
     var linexEnd = 980;
     var linexIncremented = linexStart;
-    var liney = 655;
+    var liney = 675;
     var interval = 48;
     var currentNumber = -11;
 
@@ -95,23 +95,19 @@ function generateNumberLine(){
     {
         if (currentNumber == -1 || currentNumber == -11 || currentNumber == 9)
         {
-            graphics.moveTo(linexIncremented, liney-50);
-            graphics.lineTo(linexIncremented, liney+50);
+            graphics.moveTo(linexIncremented, liney-30);
+            graphics.lineTo(linexIncremented, liney+30);
         }
         else
         {
-            graphics.moveTo(linexIncremented, liney-30);
-            graphics.lineTo(linexIncremented, liney+30);
+            graphics.moveTo(linexIncremented, liney-15);
+            graphics.lineTo(linexIncremented, liney+15);
         }
 
         linexIncremented += 48;
     }
-    
-        var style = {
-            font : 'bold 25px Verdana',
-            fill : '#000000',
-        };
-        var zero = new PIXI.Text('0', style);
+
+        var zero = new PIXI.Text('0', {font: 'bold 25px Verdana'});
         zero.x = 491;
         zero.y = 710;
         stage.addChild(zero);
@@ -120,13 +116,24 @@ function generateNumberLine(){
     stage.addChild(graphics);
 }
 
-// start animating
-animate();
+
+window.onload = function()
+{
+    renderer = PIXI.autoDetectRenderer(1000, 750,{backgroundColor : 0x1099bb});
+    document.getElementById('game').appendChild(renderer.view);
+    // create the root of the scene graph
+    stage = new PIXI.Container();
+
+    // call first scene
+    startMenu();
+
+    // start animating
+    animate();
+}
+
+
 function animate() {
     requestAnimationFrame(animate);
-
-    // just for fun, let's rotate mr rabbit a little
-    bunny.rotation += 0.1;
 
     // render the container
     renderer.render(stage);
